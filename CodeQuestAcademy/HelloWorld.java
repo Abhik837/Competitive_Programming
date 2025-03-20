36428e062fc8e788b29bcc787edefcca566bb94a
Abhik837 <abhiraam.kandula@gmail.com>
2025-03-20 12:02:34 -0400

    Create HelloWorld.java

---
+++ CodeQuestAcademy/HelloWorld.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class HelloWorld {
+    private static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        int cases = Integer.parseInt(in.nextLine());
+        for(int i = 0; i < cases; i++)
+        {
+            System.out.println(in.nextLine());
+        }
+    }
+}

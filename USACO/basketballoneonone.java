21cd2f9b995ff65d8b8e9ec0f64c505de1e2d188
Abhik837 <abhiraam.kandula@gmail.com>
2025-03-20 12:00:57 -0400

    Create basketballoneonone.java

---
+++ USACO/basketballoneonone.java
@@ -0,0 +1,11 @@
+//basketballoneonone solution for usaco.guide
+import java.util.*;
+
+public class basketballoneonone
+{
+    private static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        String input = in.nextLine();
+        System.out.println(input.substring(input.length() - 2, input.length() - 1));
+    }
+}

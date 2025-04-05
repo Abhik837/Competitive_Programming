7e3151196d0f5c9d47ff239777891f374f4cdc48
Abhik837 <abhiraam.kandula@gmail.com>
2025-04-04 21:13:22 -0400

    Create ProblemLSecretBits.java

---
+++ UVAHSPC2024/ProblemLSecretBits.java
@@ -0,0 +1,99 @@
+import java.io.*;
+import java.text.DecimalFormat;
+import java.util.*;
+
+public class ProblemLSecretBits
+{
+    static class FastReader {
+        BufferedReader b;
+        StringTokenizer s;
+
+        public FastReader() {
+            b = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (s == null || !s.hasMoreElements()) {
+                try {
+                    String line = b.readLine();
+                    if (line == null) {
+                        throw new NoSuchElementException("No input available");
+                    }
+                    s = new StringTokenizer(line);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return s.nextToken();
+        }
+
+        int nextInt() {
+            try {
+                return Integer.parseInt(next());
+            } catch (NumberFormatException e) {
+                throw new InputMismatchException("Invalid integer input");
+            }
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                if (s != null && s.hasMoreTokens()) {
+                    str = s.nextToken("\n");
+                } else {
+                    str = b.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static FastReader sc = new FastReader();
+    public static StringBuilder out = new StringBuilder();
+    public static DecimalFormat round = new DecimalFormat("#.00");
+    public static double EPS = 1e-6;
+    public static int MOD = (int) (1e9 + 7);
+    public static int[] dx = { 0, 0, 1, -1 };
+    public static int[] dy = { 1, -1, 0, 0 };
+
+    public static void dbg(String varName, Object value) {
+        int lineNumber = Thread.currentThread().getStackTrace()[2].getLineNumber();
+        System.err.println(lineNumber + ": " + varName + " = " + value);
+    }
+
+    public static void dbg(String varName, Object[] arr) {
+        int lineNumber = Thread.currentThread().getStackTrace()[2].getLineNumber();
+        System.err.println(lineNumber + ": " + varName + " = " + Arrays.toString(arr));
+    }
+
+    public static void main(String[] args) 
+    {
+        int length = sc.nextInt();
+        String bits = sc.nextLine();
+
+        for(int i = 0; i <= length - 2; i++)
+        {
+            if(bits.substring(i, i+2 ).equals("00"))
+            {
+                bits = bits.substring(0, i) + "11" + bits.substring(i+2);
+            }
+            if(bits.substring(i, i+2 ).equals("01"))
+            {
+                bits = bits.substring(0, i) + "10" + bits.substring(i+2);
+            }
+        }
+        
+        out.append(bits);
+        System.out.println(out.toString());
+    }
+}
